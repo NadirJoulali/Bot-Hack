e555e89933ebca47dbf38e7d3be554f589432f7a
NadirJoulali <39174113+NadirJoulali@users.noreply.github.com>
2018-10-31 21:55:53 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3486,5 +3486,44 @@ client.on("message", (message) => {
  
 });
 
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("498244857459114031");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+ 
+ 
+ 
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("503165105626939403");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("498244857459114031");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;        
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
 
 client.login(process.env.BOT_TOKEN);
